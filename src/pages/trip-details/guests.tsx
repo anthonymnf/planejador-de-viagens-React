eed778c615528f2b4da8ea66849bc01ff4d667ac
anthonymnf <anthonymnf30@gmail.com>
2024-07-15 08:28:51 -0300

    :sparkles: feat: Listagem dos convidados

---
+++ src/pages/trip-details/guests.tsx
@@ -0,0 +1,56 @@
+import { CheckCircle2Icon, CircleDashedIcon, UserCogIcon } from "lucide-react";
+import Button from "../../components/button";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { api } from "../../lib/axios";
+
+interface Participants {
+  id: string;
+  name: string | null;
+  email: string;
+  is_confirmed: boolean;
+}
+
+export default function Guests() {
+  const { tripId } = useParams();
+  const [participants, setParticipants] = useState<Participants[]>([]);
+  useEffect(() => {
+    api
+      .get(`/trips/${tripId}/participants`)
+      .then((response) => setParticipants(response.data.participants));
+  }, [tripId]);
+  return (
+    <div className="space-y-6 ">
+      <h2 className="font-semibold text-xl">Convidados</h2>
+      <div className="space-y-5">
+        {participants.map((participant, index) => {
+          return (
+            <div
+              key={participant.id}
+              className="flex items-center justify-between"
+            >
+              <div className="space-y-1.5">
+                <span className="text-zinc-100 block font-medium">
+                  {participant.name ?? `Convidado ${index}`}
+                </span>
+                <span className="text-zinc-400 block text-sm truncate">
+                  {participant.email}
+                </span>
+              </div>
+              {participant.is_confirmed ? (
+                <CheckCircle2Icon className="size-5 text-green-400 shrink-0" />
+              ) : (
+                <CircleDashedIcon className="size-5 text-zinc-400 shrink-0" />
+              )}
+            </div>
+          );
+        })}
+      </div>
+
+      <Button variant="secondary" size="full">
+        <UserCogIcon className="size-5" />
+        Gerenciar convidados
+      </Button>
+    </div>
+  );
+}
