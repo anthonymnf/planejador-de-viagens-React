58b596b162abffc63766b640f24cbb7452f9369b
anthonymnf <anthonymnf30@gmail.com>
2024-07-15 08:28:23 -0300

    :sparkles: feat: Header da página trip details

---
+++ src/pages/trip-details/destinitation-and-date-header.tsx
@@ -0,0 +1,45 @@
+import { MapPinIcon, CalendarIcon, Settings2Icon } from "lucide-react";
+import Button from "../../components/button";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { api } from "../../lib/axios";
+import { format } from "date-fns";
+
+interface Trip {
+  id: string;
+  destination: string;
+  ends_at: string;
+  starts_at: string;
+  is_confirmed: boolean;
+}
+
+export default function DestinitationAndDateHeader() {
+  const { tripId } = useParams();
+  const [trip, setTrip] = useState<Trip | undefined>();
+  useEffect(() => {
+    api.get(`/trips/${tripId}`).then((response) => setTrip(response.data.trip));
+  }, [tripId]);
+  const displayedDate = trip
+    ? format(trip.starts_at, "d' de 'LLL")
+        .concat(" até ")
+        .concat(format(trip.ends_at, "d' de 'LLL"))
+    : null;
+  return (
+    <div className="px-4 h-16 bg-zinc-900 rounded-xl flex justify-between items-center shadow-shape">
+      <div className="flex items-center gap-2">
+        <MapPinIcon className="size-5 text-zinc-400" />
+        <span className="text-zinc-100">{trip?.destination}</span>
+      </div>
+      <div className="flex items-center gap-5">
+        <div className="flex items-center gap-2">
+          <CalendarIcon className="size-5 text-zinc-400" />
+          <span className="text-zinc-100">{displayedDate}</span>
+        </div>
+        <div className="bg-zinc-800 w-px h-6" />
+        <Button variant="secondary">
+          Alterar local/data <Settings2Icon className="size-5" />
+        </Button>
+      </div>
+    </div>
+  );
+}
