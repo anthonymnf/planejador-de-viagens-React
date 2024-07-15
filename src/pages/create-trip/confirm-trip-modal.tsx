75d302df84195db13299b0a468f6e580043f64b7
anthonymnf <anthonymnf30@gmail.com>
2024-07-15 08:23:51 -0300

    :sparkles: feat: Modais da tela de cadastro

---
+++ src/pages/create-trip/confirm-trip-modal.tsx
@@ -0,0 +1,72 @@
+import { MailIcon, UserIcon, XIcon } from "lucide-react";
+import { FormEvent } from "react";
+import Button from "../../components/button";
+
+interface ConfirmTripModalProps {
+  closeConfirmTripModal: () => void;
+  createTrip: (event: FormEvent<HTMLFormElement>) => void;
+  setOwnerName: (name: string) => void;
+  setOwnerEmail: (email: string) => void;
+}
+
+export default function ConfirmTripModal({
+  closeConfirmTripModal,
+  createTrip,
+  setOwnerEmail,
+  setOwnerName,
+}: ConfirmTripModalProps) {
+  return (
+    <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+      <div className="w-[645px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+        <div className="space-y-2">
+          <div className="flex items-center justify-between">
+            <h2 className="text-lg font-semibold">
+              Confirmar criação de viagem
+            </h2>
+
+            <button type="button" onClick={closeConfirmTripModal}>
+              <XIcon className="size-5 text-zinc-400" />
+            </button>
+          </div>
+          <p className="text-sm text-zinc-400">
+            Para concluir a criação da viagem para{" "}
+            <span className="font-semibold text-zinc-100">
+              Florianópolis, Brasil
+            </span>{" "}
+            nas datas de{" "}
+            <span className="font-semibold text-zinc-100">
+              16 a 27 de Agosto de 2024
+            </span>{" "}
+            preencha seus dados abaixo:
+          </p>
+        </div>
+
+        <form onSubmit={createTrip} className="space-y-3">
+          <div className="bg-zinc-950 h-14 px-4 border border-zinc-800 rounded-lg flex items-center gap-2">
+            <UserIcon className="size-5 text-zinc-400 " />
+            <input
+              type="text"
+              name="name"
+              placeholder="Seu nome completo"
+              className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+              onChange={(event) => setOwnerName(event.target.value)}
+            />
+          </div>
+          <div className="bg-zinc-950 h-14 px-4 border border-zinc-800 rounded-lg flex items-center gap-2">
+            <MailIcon className="size-5 text-zinc-400 " />
+            <input
+              type="email"
+              name="email"
+              placeholder="Seu email pessoal"
+              className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+              onChange={(event) => setOwnerEmail(event.target.value)}
+            />
+          </div>
+          <Button type="submit" size="full">
+            Confirmar criação da viagem
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/create-trip/invite-guests-modal.tsx
@@ -0,0 +1,72 @@
+import { AtSignIcon, PlusIcon, XIcon } from "lucide-react";
+import { FormEvent } from "react";
+import Button from "../../components/button";
+
+interface InviteGuestsModalProps {
+  closeGuestsModal: () => void;
+  emailToInvite: string[];
+  addNewEmailToInvite: (event: FormEvent<HTMLFormElement>) => void;
+  removeEmailToInvites: (email: string) => void;
+}
+
+export default function InviteGuestsModal({
+  addNewEmailToInvite,
+  closeGuestsModal,
+  emailToInvite,
+  removeEmailToInvites,
+}: InviteGuestsModalProps) {
+  return (
+    <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+      <div className="w-[645px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+        <div className="space-y-2">
+          <div className="flex items-center justify-between">
+            <h2 className="text-lg font-semibold">Selecionar convidados</h2>
+            <button type="button" onClick={closeGuestsModal}>
+              <XIcon className="size-5 text-zinc-400" />
+            </button>
+          </div>
+          <p className="text-sm text-zinc-400">
+            Os convidados irão receber e-mails para confirmar a participação na
+            viagem.
+          </p>
+        </div>
+        <div className="flex flex-wrap gap-2">
+          {emailToInvite.map((email) => {
+            return (
+              <div
+                key={email}
+                className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2"
+              >
+                <span className="text-zinc-300">{email}</span>
+                <button
+                  type="button"
+                  onClick={() => removeEmailToInvites(email)}
+                >
+                  <XIcon className="size-4 text-zinc-400" />
+                </button>
+              </div>
+            );
+          })}
+        </div>
+        <div className="w-full h-px bg-zinc-800" />
+        <form
+          onSubmit={addNewEmailToInvite}
+          className="bg-zinc-950 p-2.5 border border-zinc-800 rounded-lg flex items-center gap-2"
+        >
+          <div className="px-2 flex items-center flex-1 gap-2">
+            <AtSignIcon className="size-5 text-zinc-400 " />
+            <input
+              type="email"
+              name="email"
+              placeholder="Digite o email do convidado"
+              className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+            />
+          </div>
+          <Button type="submit">
+            Convidar <PlusIcon className="size-5" />
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+}
