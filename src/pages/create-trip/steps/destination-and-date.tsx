17036e36b21cb4fc73e5486a6c931af34201301f
anthonymnf <anthonymnf30@gmail.com>
2024-07-15 08:24:39 -0300

    :sparkles: feat: Criação do primeiro step do formulário

---
+++ src/pages/create-trip/steps/destination-and-date.tsx
@@ -0,0 +1,106 @@
+import {
+  ArrowRightIcon,
+  Calendar,
+  MapPinIcon,
+  Settings2,
+  XIcon,
+} from "lucide-react";
+import Button from "../../../components/button";
+import { useState } from "react";
+import { DateRange, DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+import { format } from "date-fns";
+
+interface DestinationAndDateProps {
+  isGuestsInputOpen: boolean;
+  eventStartAndEndDates: DateRange | undefined;
+  closeGuestsInput: () => void;
+  openGuestsInput: () => void;
+  setDestination: (destination: string) => void;
+  setEventStartAndEndDates: (dates: DateRange | undefined) => void;
+}
+
+export default function DestinationAndDate({
+  closeGuestsInput,
+  isGuestsInputOpen,
+  openGuestsInput,
+  setDestination,
+  setEventStartAndEndDates,
+  eventStartAndEndDates,
+}: DestinationAndDateProps) {
+  const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
+
+  function openDatePicker() {
+    setIsDatePickerOpen(true);
+  }
+  function closeDatePicker() {
+    setIsDatePickerOpen(false);
+  }
+
+  const displayedDate =
+    eventStartAndEndDates &&
+    eventStartAndEndDates.from &&
+    eventStartAndEndDates.to
+      ? format(eventStartAndEndDates.from, "d' de 'LLL")
+          .concat(" até ")
+          .concat(format(eventStartAndEndDates.to, "d' de 'LLL"))
+      : null;
+  return (
+    <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center gap-3 shadow-shape">
+      <div className="flex items-center gap-2 flex-1">
+        <MapPinIcon className="size-5 text-zinc-400 " />
+        <input
+          type="text"
+          placeholder="Para onde você vai?"
+          className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+          disabled={isGuestsInputOpen}
+          onChange={(event) => setDestination(event.target.value)}
+        />
+      </div>
+      <button
+        onClick={openDatePicker}
+        disabled={isGuestsInputOpen}
+        className="flex items-center gap-2 text-left w-[200px] "
+      >
+        <Calendar className="size-5 text-zinc-400 " />
+        <span className="text-lg text-zinc-400  w-40 flex-1">
+          {displayedDate || "Quando?"}
+        </span>
+      </button>
+
+      {isDatePickerOpen && (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+          <div className="rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <h2 className="text-lg font-semibold">Selecione a data</h2>
+
+                <button type="button" onClick={closeDatePicker}>
+                  <XIcon className="size-5 text-zinc-400" />
+                </button>
+              </div>
+            </div>
+
+            <DayPicker
+              mode="range"
+              selected={eventStartAndEndDates}
+              onSelect={setEventStartAndEndDates}
+            />
+          </div>
+        </div>
+      )}
+
+      <div className="w-px h-6 bg-zinc-500" />
+
+      {isGuestsInputOpen ? (
+        <Button onClick={closeGuestsInput} variant="secondary">
+          Alterar local/data <Settings2 className="size-5" />
+        </Button>
+      ) : (
+        <Button onClick={openGuestsInput}>
+          Continuar <ArrowRightIcon className="size-5" />
+        </Button>
+      )}
+    </div>
+  );
+}
